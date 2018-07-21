15f21e99c52c043c471ab90dea5c26d0787992f8
SATTIA Dhinesh <dhinesh@live.fr>
2018-07-21 16:51:22 +0200

    init

---
+++ script.js
@@ -0,0 +1,22 @@
+function sendVal(x) {
+    document.getElementById("pwd").value += x;
+}
+
+function shuffle(a) {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+}
+function deletePwd() {
+    document.getElementById('pwd').value = '';
+}
+function deleteLastChar() {
+    let val = document.getElementById('pwd').value;
+    document.getElementById('pwd').value = val.substr(0, val.length - 1);
+  }
+function send() {
+    console.log("ok");
+}
+
