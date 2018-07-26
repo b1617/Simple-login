51633fad89d7df9cc20281ff870fb8ddce3e8a7c
SATTIA Dhinesh <dhinesh@live.fr>
2018-07-26 14:23:45 +0200

    update password required

--- script.js
+++ script.js
@@ -15,6 +15,17 @@ function deletePwd() {
 function deleteLastChar() {
     let val = document.getElementById('pwd').value;
     document.getElementById('pwd').value = val.substr(0, val.length - 1);
-  }
+}
 
+function send() {
+    let email = document.getElementById('email').value;
+    let pwd = document.getElementById('pwd').value;
+    if (pwd == '') {
+        alert('add password');
+        return false;
+
+    } else {
+        alert(email + " "  + pwd);
+    }
+}
 
